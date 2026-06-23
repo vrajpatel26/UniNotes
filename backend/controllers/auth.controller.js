81768a7bc206fc88a97f9bdb6dbbf91893c13503
vrajpatel26 <vraj.26112004@gmail.com>
2026-06-24 02:41:19 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -36,7 +36,7 @@ export const signup = async (req, res) => {
         res.cookie("token", token, ({
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: none,
+            sameSite: "None",
             secure: true
         }))
 
@@ -88,7 +88,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, ({
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: none,
+            sameSite: "None",
             secure: true
         }))
 
