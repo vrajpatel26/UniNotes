b7cf036b100d04882ce5e122397e673f17a30def
vrajpatel26 <vraj.26112004@gmail.com>
2026-06-26 01:04:55 +0530

    Update feedback.controller.js

--- backend/controllers/feedback.controller.js
+++ backend/controllers/feedback.controller.js
@@ -18,15 +18,13 @@ export const sendFeedback = async (req, res) => {
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
+            family: 4, // Force IPv4
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
-            connectionTimeout: 10000,
-            greetingTimeout: 10000,
-            socketTimeout: 10000,
         });
 
         console.log("5. Transport created");
